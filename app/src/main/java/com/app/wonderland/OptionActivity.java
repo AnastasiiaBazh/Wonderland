2c298024ad1b5b4555a8777a69c41330d06ceb2c
anastasiiabazh <bazhenova.a.s@gmail.com>
2023-12-04 16:59:38 +0000

    Add style and name for activity_options

--- app/src/main/java/com/app/wonderland/OptionActivity.java
+++ app/src/main/java/com/app/wonderland/OptionActivity.java
@@ -1,5 +1,6 @@
 package com.app.wonderland;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ public class OptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.primary)));
+        getSupportActionBar().setTitle(R.string.options_name);
     }
 }
 
